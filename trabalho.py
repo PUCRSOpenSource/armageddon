2afa874212077b5cb43ad850e384e0411296be2c
Matthias Nunes <matthiasnunes@gmail.com>
2019-06-15 17:24:56 -0300

    Reads csv file and organizes by class

--- trabalho.py
+++ trabalho.py
@@ -1,5 +1,29 @@
 from treetagger import TreeTagger
 from pprint import pprint
 
-tt_pt = TreeTagger(language='portuguese2')
-pprint(tt_pt.tag('Minha namorada é maravilhosa, gostosa, musa do verão!'))
+import csv
+import json
+
+saco_de_gato = {}
+
+with open('todos.csv', 'r', encoding='utf-8', newline='') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+            pergunta = {}
+            pergunta['id'] = row[0]
+            pergunta['pergunta'] = row[1]
+            pergunta['resposta'] = row[2]
+            pergunta['classe'] = row[3]
+            saco_de_gato.setdefault(row[3], []).append(pergunta)
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+
+print(json.dumps(saco_de_gato, indent = 4, ensure_ascii = False))
+
+
+# tt_pt = TreeTagger(language = 'portuguese2')
+# pprint(tt_pt.tag('Minha namorada é maravilhosa, gostosa, musa do verão!'))
