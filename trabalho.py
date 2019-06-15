31e26b73a3e6ab6d063ba4b62f45f91288317ec7
Matthias Nunes <matthiasnunes@gmail.com>
2019-06-15 18:50:31 -0300

    Calls tree tagger and adds its return to the bag of cats

--- trabalho.py
+++ trabalho.py
@@ -5,25 +5,25 @@ import csv
 import json
 
 saco_de_gato = {}
+tt_pt = TreeTagger(language = 'portuguese2')
 
-with open('todos.csv', 'r', encoding='utf-8', newline='') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ',')
+# ID;PERGUNTAS;RESPOSTAS;CLASSES;;
+with open('parcial.csv', 'r', encoding='utf-8', newline='') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ';')
     line_count = 0
     for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-        else:
-            pergunta = {}
-            pergunta['id'] = row[0]
-            pergunta['pergunta'] = row[1]
-            pergunta['resposta'] = row[2]
-            pergunta['classe'] = row[3]
-            saco_de_gato.setdefault(row[3], []).append(pergunta)
-            line_count += 1
+        pergunta = {}
+        pergunta['id'] = row[0]
+        pergunta['pergunta'] = row[1]
+        pergunta['resposta'] = row[2]
+        pergunta['classe'] = row[3]
+        saco_de_gato.setdefault(row[3], []).append(pergunta)
+        line_count += 1
     print(f'Processed {line_count} lines.')
+
+for classe, perguntas in saco_de_gato.items():
+    for pergunta in perguntas:
+        pergunta['morfologia'] = tt_pt.tag(pergunta['pergunta'])
 
 print(json.dumps(saco_de_gato, indent = 4, ensure_ascii = False))
 
-
-# tt_pt = TreeTagger(language = 'portuguese2')
-# pprint(tt_pt.tag('Minha namorada é maravilhosa, gostosa, musa do verão!'))
