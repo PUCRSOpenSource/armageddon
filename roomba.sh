f4db63a3bedfbe14f5eb51663613c00a3a2c6a41
Matthias Nunes <matthiasnunes@gmail.com>
2019-06-16 15:17:06 -0300

    Melhora o roomba

--- roomba.sh
+++ roomba.sh
@@ -1,3 +1,11 @@
 #! /usr/bin/bash
 
-sed -i -e 's/random forest/floresta aleatória/gI' -e 's/decision tree/árvore de decisão/gI' $1
+sed -i \
+    -e 's/random forest/floresta aleatória/gI' \
+    -e 's/decision tree/árvore de decisão/gI' \
+    -e 's/emprestimos/empréstimos/gI' \
+    -e 's/varios/vários/gI' \
+    -e 's/aplicacao/aplicação/gI' \
+    -e 's/calculos/cálculos/gI' \
+    -e 's/maioritária/majoritária/gI' \
+    $1
